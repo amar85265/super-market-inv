02ae4d5eb5fcf25365a753c89a50be9ad848df9c
Manisarathia <manisarathi.a@gmail.com>
2026-05-19 13:08:28 +0530

    Add files via upload

---
+++ src/main/java/com/example/InventoryManagementSystem/Dto/CategoryDAOImpl.java
@@ -0,0 +1,50 @@
+package com.example.InventoryManagementSystem.dao;
+
+import com.example.InventoryManagementSystem.model.Category;
+import com.example.InventoryManagementSystem.repository.CategoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CategoryDAOImpl implements CategoryDAO {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Override
+    public Category saveCategory(Category category) {
+        return repository.save(category);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Category> getCategoryById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Category updateCategory(Long id, Category category) {
+
+        Category existingCategory = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        existingCategory.setCategoryName(category.getCategoryName());
+        existingCategory.setDescription(category.getDescription());
+        existingCategory.setStatus(category.getStatus());
+
+        return repository.save(existingCategory);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        repository.deleteById(id);
+    }
+}
