2ab116b7ae7210432d936d630990d8921c22a7bc
Manisarathi A <manisarathi.a@gmail.com>
2026-07-03 17:54:24 +0530

    Add files via upload

--- src/main/java/com/example/InventoryManagementSystem/Repository/ProductRepository.java
+++ src/main/java/com/example/InventoryManagementSystem/Repository/ProductRepository.java
@@ -5,9 +5,6 @@ import com.example.InventoryManagementSystem.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByStockQuantityLessThanEqual(Integer minimumStock);
 }
