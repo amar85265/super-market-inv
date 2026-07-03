81dca589e28cff8031ef551ad5a47ed183931f0e
Manisarathi A <manisarathi.a@gmail.com>
2026-07-03 17:57:53 +0530

    Add files via upload

--- src/main/java/com/example/InventoryManagementSystem/dto/PaymentTransactionRequestDTO.java
+++ src/main/java/com/example/InventoryManagementSystem/dto/PaymentTransactionRequestDTO.java
@@ -1,16 +1,12 @@
 package com.example.InventoryManagementSystem.dto;
 
 import lombok.Data;
-
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 
 @Data
 public class PaymentTransactionRequestDTO {
-
     private Long invoiceId;
     private String paymentMethod;
     private String transactionReference;
     private BigDecimal amount;
-    private OffsetDateTime paymentDate;
 }
