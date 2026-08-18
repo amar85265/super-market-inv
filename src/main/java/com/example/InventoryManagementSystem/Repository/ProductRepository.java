74c7d5a4e506ff2b663c6a4eca49c244f9fc4db4
amar85265 <1998amardeepu@gmail.com>
2026-08-18 23:34:34 +0530

    commit

--- src/main/java/com/example/InventoryManagementSystem/Repository/ProductRepository.java
+++ src/main/java/com/example/InventoryManagementSystem/Repository/ProductRepository.java
@@ -4,6 +4,12 @@ import com.example.InventoryManagementSystem.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Optional<Product> findBySku(String sku);
+
+    Optional<Product> findByBarcode(String barcode);
 }
