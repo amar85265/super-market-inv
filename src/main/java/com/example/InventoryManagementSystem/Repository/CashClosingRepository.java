f31c8f6e3263c676383d1c4fc015f461e5ede42d
amar85265 <1998amardeepu@gmail.com>
2026-08-19 18:19:07 +0530

    Delete src/main/java/com/example/InventoryManagementSystem/Repository/CashClosingRepository.java

--- src/main/java/com/example/InventoryManagementSystem/Repository/CashClosingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.InventoryManagementSystem.Repository;
-
-import com.example.InventoryManagementSystem.model.CashClosing;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CashClosingRepository
-        extends JpaRepository<CashClosing, Long> {
-}
