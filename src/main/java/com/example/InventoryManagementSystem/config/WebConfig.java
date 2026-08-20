2d843b53225f2f848dc9339c57bede9d71827ecf
amar85265 <1998amardeepu@gmail.com>
2026-08-20 19:27:59 +0530

    commit

--- src/main/java/com/example/InventoryManagementSystem/config/WebConfig.java
+++ src/main/java/com/example/InventoryManagementSystem/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // your Vite dev origin
+                .allowedOrigins("*") // your Vite dev origin
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
